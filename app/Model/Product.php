ff9ac3b098c2177ce7b16c81bfa68ea336d6439d
Aushraful <aushraful@gmail.com>
2019-08-20 17:17:16 +0600

    relationship created between product and review

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,9 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
     protected $fillable = ['name','detail','price','stock','discount'];
     protected $table = 'products';
+
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,9 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
     protected $fillable = ['product_id','customer','star','review'];
     protected $table = 'reviews';
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
