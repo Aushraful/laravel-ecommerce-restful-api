6fbe3ba6829f213236c0ab93c6cef3c9e7b84167
Aushraful <aushraful@gmail.com>
2019-08-21 00:38:32 +0600

    Exception Handling done

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+use Cartalyst\Stripe\Exception\NotFoundException;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +50,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->expectsJson()){
+            if ($exception instanceof ModelNotFoundException){
+                return response()->json(['errors' => 'Product not found'], Response::HTTP_NOT_FOUND);
+            }
+            if ($exception instanceof NotFoundHttpException){
+                return response()->json(['errors' => 'Page not found'], Response::HTTP_NOT_FOUND);
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
