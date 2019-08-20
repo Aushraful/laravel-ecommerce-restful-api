f420f54823cebc64b92a1efc035e1e1a85beb819
Aushraful <aushraful@gmail.com>
2019-08-20 16:33:37 +0600

    migration created for products and review table

--- database/migrations/2019_08_20_094133_create_reviews_table.php
+++ database/migrations/2019_08_20_094133_create_reviews_table.php
@@ -15,6 +15,11 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->string('review');
+            $table->string('star');
             $table->timestamps();
         });
     }
