1e73ebefb6a2e6a577bfaa04e7ce8c48463bd8f5
Aushraful <aushraful@gmail.com>
2019-08-20 19:48:07 +0600

    Product Collection Resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Resources\Product;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +15,15 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+//        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'total_price' => round( ( ( $this->discount / 100 ) * $this->price ),2 ),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'no reviews yet!',
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
     }
 }
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'total_price' => round( ( ( $this->discount / 100 ) * $this->price ),2 ),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'no reviews yet!',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
